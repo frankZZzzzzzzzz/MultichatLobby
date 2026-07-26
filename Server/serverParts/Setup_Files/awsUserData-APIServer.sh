6ac7a44bf5a5b07d1a7b550971c04c0b5d3a78b7
Frank Zhang <frankz14152@gmail.com>
2026-07-26 16:52:50 -0400

    Fix

---
+++ Server/serverParts/Setup_Files/awsUserData-APIServer.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+# Configuration 
+export DATABASE_IP=10.0.4.53
+export DATABASE_PORT=3000
+export API_SERVER_PORT=3000
+
+REPO_URL="https://github.com/frankZZzzzzzzzz/MultichatLobby"
+PROJECT_DIR="$HOME/multichat-lobby"
+
+# Update system
+echo "Updating system packages..."
+sudo apt update
+
+# Install Git
+echo "Installing Git..."
+sudo apt install -y git
+
+# Install Node.js and npm
+echo "Installing Node.js and npm..."
+sudo apt install -y nodejs npm
+
+# Install PM2 globally
+echo "Installing PM2 Globally"
+sudo npm install -g pm2
+
+# Clone/Update repository
+if [ -d "$PROJECT_DIR" ]; then
+    echo "Project directory already exists."
+    echo "Pulling latest changes..."
+
+    cd "$PROJECT_DIR"
+    git pull
+else
+    echo "Cloning repository..."
+
+    git clone "$REPO_URL" "$PROJECT_DIR"
+    cd "$PROJECT_DIR"
+fi
+
+# Install Server dependencies
+echo "Installing server dependencies..."
+cd "$PROJECT_DIR/Server"
+npm ci
+
+echo "Server Setup complete"
+
+# Run Server
+cd "$PROJECT_DIR/Server/serverParts"
+pm2 start APIServer.js --name API-Server
+pm2 save
